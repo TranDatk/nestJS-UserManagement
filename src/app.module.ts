45cb63c17a88a88d62262c04819c82ad8072a2f6
TranDatk <hgooshvhd123@gmail.com>
2024-06-16 21:38:44 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { softDeletePlugin } from 'soft-delete-plugin-mongoose';
+import { FilesModule } from './files/files.module';
 
 @Module({
   imports: [
@@ -28,6 +29,7 @@ import { softDeletePlugin } from 'soft-delete-plugin-mongoose';
     ),
     UsersModule,
     AuthModule,
+    FilesModule,
   ],
 
   controllers: [AppController],
